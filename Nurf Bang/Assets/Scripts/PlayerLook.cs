8006b02816545d1ae384ef8d145357808ff1f0c0
lip ables <phillipables1@gmail.com>
2018-04-03 15:47:31 -0400

    RotateCamera function called in UpDate method, gets float inputs for x and y mouse, a public variable to control mouse rotate speed, float for rotate amount which is our mouse input with our rotate speed as a multiplier

--- Nurf Bang/Assets/Scripts/PlayerLook.cs
+++ Nurf Bang/Assets/Scripts/PlayerLook.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //new class scope variable, public so we can interact directly in the inspector
+    //controls the speed at which we rotate when we move the mouse
+    public float mouseSensitivity;
+
+    void Update()
+    {
+
+
+        RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        //declare two variables
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        //mouseSensitivity as a multiplier
+        float rotAmountX = MouseX * mouseSensitivity;
+        float rotAmountY = MouseY * mouseSensitivity;
+
+    }
 }
