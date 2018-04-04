06736bcd74065eb44d7f4558255816025adbc099
lip ables <phillipables1@gmail.com>
2018-04-04 18:43:43 -0400

    add component CharacterController to player object and on Awake get public CharacterController reference to it

--- Nurf Bang/Assets/Scripts/PlayerMove.cs
+++ Nurf Bang/Assets/Scripts/PlayerMove.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController charController;
 
+    void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        
+    }
 }
