97a3ecbd531e4880d898262ce4af884d73d21e1d
lip ables <phillipables1@gmail.com>
2018-04-04 22:38:13 -0400

    on Awake GetComponent Rigidbody, MovePlayer update if KeyCode.Space down AddForce .up * jumpForce to our Rigidbody -> ForceMode.Impusle

--- Nurf Bang/Assets/Scripts/PlayerMove.cs
+++ Nurf Bang/Assets/Scripts/PlayerMove.cs
@@ -12,12 +12,14 @@ public class PlayerMove : MonoBehaviour
     public LayerMask groundLayers;
     public float jumpForce = 7;  // how high we want to jump
     public CapsuleCollider col;
+    public Rigidbody rb;
 
 
     void Awake()
     {
         charController = GetComponent<CharacterController>();
         col = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
@@ -34,5 +36,13 @@ public class PlayerMove : MonoBehaviour
 
         charController.SimpleMove(moveDirForward);
         charController.SimpleMove(moveDirSide);
+
+
+        //jump code
+        //when we hit space add force
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);  // ForceMode.Impulse is very important or else you wont see much happen
+        };
     }
 }
