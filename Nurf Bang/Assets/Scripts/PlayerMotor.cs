ac838c765327a40f8c1b0e345046a87a248c3fbf
lip ables <phillipables1@gmail.com>
2018-04-14 15:24:13 -0400

    private floats for verticalvelocity jumpforce and gravity, on start get component type charachter controller

--- Nurf Bang/Assets/Scripts/PlayerMotor.cs
+++ Nurf Bang/Assets/Scripts/PlayerMotor.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour {
+    private CharacterController character;
 
+    private float verticalVelocity;
+    private float gravity = 14.0f;
+    private float jumpForce = 10.0f;
+
+    private void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
 }
