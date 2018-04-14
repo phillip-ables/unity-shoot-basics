4cbb5189ae7874f69b83c642cdfdede6c4f6ac27
lip ables <phillipables1@gmail.com>
2018-04-14 15:14:31 -0400

    new jump tutorial and removed old player renamed it to PlayerDoesntWork Prefab, maybe one day i can fix it, now a simpple floating cube with collider removed and a character controller added

---
+++ Nurf Bang/Assets/Scripts/DontWork/PlayerLook.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+    public Transform playerBody;
+    //new class scope variable, public so we can interact directly in the inspector
+    public float mouseSensitivity;
+
+    float xAxisClamp = 0.0f;
+
+    //lock the mouse cursor to the center of the screen so that it cannot move
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        RotateCamera();
+    }
+    void RotateCamera()
+    {
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        //mouseSensitivity as a multiplier
+        float rotAmountY = MouseX * mouseSensitivity * Time.deltaTime;
+        float rotAmountX = MouseY * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountX;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountY;
+
+        if(xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if(xAxisClamp < -90) { xAxisClamp = -90; targetRotCam.x = 270; }
+
+        //transform.rotation is a quaternion and we use Euler angles to tranform it with vectors
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+    }
+}
---
+++ Nurf Bang/Assets/Scripts/DontWork/PlayerMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController charController;
+
+    public float walkSpeed;
+
+    //which layers we want to look out for:
+    public LayerMask groundLayers;
+    public float jumpForce = 799;  // how high we want to jump
+    public Rigidbody rb;
+
+    #region Monobehaviour API
+
+    void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 moveDirSide = transform.right * horiz * walkSpeed;
+        Vector3 moveDirForward = transform.forward * vert * walkSpeed;
+
+        charController.SimpleMove(moveDirForward);
+        charController.SimpleMove(moveDirSide);
+
+
+        //jump code
+        //when we hit space add force
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump");
+            rb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);  // ForceMode.Impulse is very important or else you wont see much happen
+        };
+    }
+    #endregion
+}
