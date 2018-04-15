f4d1dd381054df832ebe8b5a344d71fe7bec9dd2
lip ables <phillipables1@gmail.com>
2018-04-14 21:06:48 -0400

    on start lock mouse, each update get axis horizontal and vertical multiplied by speed and time and pass as transform Translate for x and z straffe and translation, if escape CursorLockMode is none

--- Nurf Bang/Assets/CharacterControls.cs
+++ Nurf Bang/Assets/CharacterControls.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterControls : MonoBehaviour {
+    public float speed = 10.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
 }
