bf3f11aa914a9c8a6a590bdaaf87ec5f99577357
lip ables <phillipables1@gmail.com>
2018-04-15 17:29:31 -0400

    went back to the original move mechanism i programed

--- Nurf Bang/Assets/HunchController.cs
+++ Nurf Bang/Assets/HunchController.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HunchController : MonoBehaviour {
-    public float speed = 20.0f;
+    public float speed = 220.0f;
+    public float jumpForce = 15.0f;
+
+    private Rigidbody rbHunch;
+    private Vector3 velHunch;
+    private float horVel;
+    private float verVel;
+
+
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
+        rbHunch = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        float translation = Input.GetAxis("Horizontal") * speed;
-        float straffe = Input.GetAxis("Vertical") * speed;
-        translation *= Time.deltaTime;
-        straffe *= Time.deltaTime;
-
-        transform.Translate(straffe, 0, -translation);
-
         if (Input.GetKeyDown("escape"))
             Cursor.lockState = CursorLockMode.None;
         
+    }
+
+    private void LateUpdate()
+    {
+        velHunch = rbHunch.velocity;
+        velHunch.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        velHunch.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && rbHunch.position.y < 1)
+        {
+            Debug.Log("Jump");
+            velHunch.y = jumpForce;
+        }
+        rbHunch.velocity = velHunch;
     }
 }
