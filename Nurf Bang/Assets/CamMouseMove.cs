994e64c07537db7dfb94376bd628c815e679f105
lip ables <phillipables1@gmail.com>
2018-04-14 23:42:14 -0400

    clear my attempt from cam mouse move script

--- Nurf Bang/Assets/CamMouseMove.cs
+++ Nurf Bang/Assets/CamMouseMove.cs
@@ -2,19 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamMouseMove : MonoBehaviour {
-    private float rotHead;
-    private float rotBody;
-    private Vector3 charachterRot;
-    public float turnSpeed = 10.0f;
+public class CamMouseMove : MonoBehaviour
+{
 
-    private void Start()
-    {
-    }
-    private void Update()
-    {
-        float mouseX = Input.GetAxis("MouseX");
-        float mouseY = Input.GetAxis("MouseY");
-
-    }
 }
