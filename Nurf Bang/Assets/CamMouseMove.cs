51baa9199577b7f17b8dd9d10dc40b819d671a35
lip ables <phillipables1@gmail.com>
2018-04-14 22:56:41 -0400

    cam mouse move script added to camera, attempt to move but forgot eulers, got input for x and y mouse, float for rot head and body, vector3 for charachter rot

---
+++ Nurf Bang/Assets/CamMouseMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMouseMove : MonoBehaviour {
+    private float rotHead;
+    private float rotBody;
+    private Vector3 charachterRot;
+    public float turnSpeed = 10.0f;
+
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("MouseX");
+        float mouseY = Input.GetAxis("MouseY");
+
+    }
+}
