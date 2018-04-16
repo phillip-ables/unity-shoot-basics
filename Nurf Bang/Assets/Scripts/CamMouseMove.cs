ec1748c8786f58fdb5dd2e7ae10d9d13ae9ba466
lip ables <phillipables1@gmail.com>
2018-04-16 16:03:19 -0400

    new models for rotation practice on test level, player has LerpRotation Script added

---
+++ Nurf Bang/Assets/Scripts/CamMouseMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMouseMove : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+    Rigidbody rbHunch;
+
+    private void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    private void Update()
+    {
+        var md = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
