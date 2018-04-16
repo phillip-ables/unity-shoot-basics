96d7951fc0712958709a778ec0477d677bd7f343
lip ables <phillipables1@gmail.com>
2018-04-16 14:22:18 -0400

    new scene test to work on eular angle concepts

---
+++ Nurf Bang/Assets/HeadMouseMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMouseMove : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject charHump;
+    Rigidbody rbHunch;
+    public Transform tranNeck;
+
+    private void Start()
+    {
+        charHump = this.transform.parent.gameObject;
+
+    }
+
+    private void Update()
+    {
+        var md = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook = smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+
+        transform.rotation  *= Quaternion.AngleAxis(-mouseLook.y, this.transform.right);
+        //transform.eulerAngles = new Vector3(-mouseLook.y, 0f, 0f);
+        charHump.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, charHump.transform.up);
+    }
+}
--- Nurf Bang/Assets/HunchController.cs
+++ Nurf Bang/Assets/HunchController.cs
@@ -10,12 +10,6 @@ public class HunchController : MonoBehaviour {
     private float horVel;
     private float verVel;
 
-    public float sensitivity = 5.0f;
-    public float smoothing = 2.0f;
-    public Transform headHunch;
-    private Vector2 mouseLook;
-    private Vector2 smoothV;
-
     private void Start()
     {
         //Cursor.lockState = CursorLockMode.Locked;
@@ -31,15 +25,6 @@ public class HunchController : MonoBehaviour {
 
     private void LateUpdate()
     {
-        //move head parent with camera child
-        var md = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-
-        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
-        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
-        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
-        mouseLook += smoothV;
-        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
-
 
         //move position of hunch empty rigid
         velHunch = rbHunch.velocity;
