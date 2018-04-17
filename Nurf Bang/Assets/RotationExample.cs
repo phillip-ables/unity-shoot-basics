e5718cf2b979c29b4593512e9c45ccdaa7a5172c
lip ables <phillipables1@gmail.com>
2018-04-16 20:54:24 -0400

    blockA now has move controls and rotation controls

--- Nurf Bang/Assets/RotationExample.cs
+++ Nurf Bang/Assets/RotationExample.cs
@@ -3,9 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotationExample : MonoBehaviour {
+    public float speed = 15.0f;
 
     private void Start()
     {
         transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + mouseY,
+                                                transform.localEulerAngles.y + mouseX,
+                                                0.0f);
+
+        transform.position = new Vector3(Input.GetAxis("horizontal") * speed,
+                                        0.0f,
+                                        Input.GetAxis("vertical") * speed);
     }
 }
