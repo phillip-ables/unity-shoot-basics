45c604fb331ddecdaeda05d36850d1862e21f15d
lip ables <phillipables1@gmail.com>
2018-04-16 21:32:55 -0400

    block a consist of my first working mouse turn for a head sphere

---
+++ Nurf Bang/Assets/Scripts/BlockAController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockAController : MonoBehaviour
+{
+    public float speed = 15.0f;
+    public float jumpForce = 15.0f;
+    public float rotSpeed = 20.0f;
+    Vector3 playerMove;
+    GameObject blockA;
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        blockA = this.transform.parent.gameObject;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + mouseY * rotSpeed,
+                                                transform.localEulerAngles.y + mouseX * rotSpeed,
+                                                0.0f);
+
+        playerMove = blockA.transform.position;
+        playerMove.x += Input.GetAxis("Horizontal") * speed;
+        playerMove.z += Input.GetAxis("Vertical") * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space) && playerMove.y < jumpForce)
+        {
+            playerMove.y = jumpForce;
+            Debug.Log("JUMP!!");
+        }
+
+        blockA.transform.position = playerMove;
+    }
+}
+
---
+++ Nurf Bang/Assets/Scripts/LerpRotation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpRotation : MonoBehaviour
+{
+    Vector3 relativePosition; // the difference in space
+    Quaternion targetRotation;
+
+    public Transform player;
+    public Transform gun;
+    public float speed = 0.1F;
+    private bool rotating = false;
+    private float rotationTime;
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //targeting phase 
+            relativePosition = player.position - gun.position;
+            targetRotation = Quaternion.LookRotation(relativePosition);
+            rotating = true;
+            rotationTime = 0.0f;
+        }
+
+
+        if (rotating)
+        {
+            //and a transition phase
+            rotationTime += Time.deltaTime * speed;
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationTime);
+        }
+
+        if (rotationTime > 1)
+            rotating = false;
+    }
+
+}
