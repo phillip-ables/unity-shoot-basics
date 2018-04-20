e733993697c25c9448b1fafe83273c14245aeb84
lip ables <phillipables1@gmail.com>
2018-04-19 21:37:12 -0400

    bulletFace has collider and rigidbody component, dont use gravity, is kinematic, is triggered collision

---
+++ Nurf Bang/Assets/HunchBotEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunchBotEnemy : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
