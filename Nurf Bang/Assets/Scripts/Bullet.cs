78d7bc60188e6ad1f76edf225f56ceed22de5e20
lip ables <phillipables1@gmail.com>
2018-04-19 21:19:24 -0400

    bullets are now a smiley face and game object hunch bot is now in the scene as enemy tag

---
+++ Nurf Bang/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float aliveTime;
+    public float damage;
+    public float moveSpeed;
+
+    //couldnt get access to scene gameObject for some reason so im gonna roll the dice and stick to the first patch that worked for me and continue the tutorial!!!
+
+    private void Update()
+    {
+        aliveTime -= Time.deltaTime;
+
+        if (aliveTime <= 0)
+            Destroy(this.gameObject);
+
+        this.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+
+    }
+}
