e864775b1626f45878db647802db760370f4acfe
lip ables <phillipables1@gmail.com>
2018-04-19 20:55:17 -0400

    fixed bug by instantiating bullet object to transform rotation of the bullet spawns rotation

--- Nurf Bang/Assets/Shooting.cs
+++ Nurf Bang/Assets/Shooting.cs
@@ -24,5 +24,6 @@ public class Shooting : MonoBehaviour {
     public void Fire()
     {
         _bullet = Instantiate(bullet.transform, bulletSpawn.transform.position, Quaternion.identity);
+        _bullet.rotation = bulletSpawn.transform.rotation;
     }
 }
