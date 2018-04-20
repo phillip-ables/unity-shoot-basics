5ee8e3b2415d23c55d3d140887ea5da4a10e4994
lip ables <phillipables1@gmail.com>
2018-04-19 22:30:55 -0400

    enemy now updates to look at player but its off by 97 degrees in the y

--- Nurf Bang/Assets/HunchBotEnemy.cs
+++ Nurf Bang/Assets/HunchBotEnemy.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class HunchBotEnemy : MonoBehaviour
 {
-
     public float health;
-    
+
+    public Transform player;
+
+    private void Update()
+    {
+        Vector3 relativePos = player.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = lookRotation;
+
+    }
+
 }
