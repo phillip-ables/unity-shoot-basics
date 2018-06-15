65cb0c29fbc84f55e5d7f54e50873d5f3e6a2fc1
lip ables <phillipables1@gmail.com>
2018-06-15 19:07:55 -0400

    on update create new vector3 pos thats equatl to player position (z minus camera z) then transform position by smooth damp from current to pos value referencing velocity and at smooth

--- Nurf Bang/Assets/TopDownBalls/Scripts/BallsCamera.cs
+++ Nurf Bang/Assets/TopDownBalls/Scripts/BallsCamera.cs
@@ -6,10 +6,19 @@ public class BallsCamera : MonoBehaviour {
     //Variables
     public Transform player;
     public float smooth = 0.3f;
+    public float cameraZ;
 
     private Vector3 velocity = Vector3.zero;
 
     //Methods
+    private void Update()
+    {
+        Vector3 pos = new Vector3();
+        pos.x = player.position.x;
+        pos.y = player.position.y;
+        pos.z = player.position.z - cameraZ;
 
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+    }
 
 }
