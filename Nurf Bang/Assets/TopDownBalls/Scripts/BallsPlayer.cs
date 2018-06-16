3242725a138d0aebed3390da532e1b5dbc0ffc71
lip ables <phillipables1@gmail.com>
2018-06-16 19:12:01 -0400

    if playerplane raycast from ray to hit distance->, get distance form ray to hitDistance, lookrotation of targetpoint minus transform position , set x and z to 0 so it doesnt roll, transform rotation slerp to target rot at rot speed

--- Nurf Bang/Assets/TopDownBalls/Scripts/BallsPlayer.cs
+++ Nurf Bang/Assets/TopDownBalls/Scripts/BallsPlayer.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class BallsPlayer : MonoBehaviour {
     //Variables
     public float movementSpeed;
+    public int rotSpeed = 7;
 
     //Methods
     private void Update()
     {
-        PlayerLook playerPlane 
+        //camera position
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        //mouse location
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDist = 0.0f;
+
+        if(playerPlane.Raycast(ray, out hitDist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotSpeed * Time.deltatime);
+        }
+
     }
 
 }
