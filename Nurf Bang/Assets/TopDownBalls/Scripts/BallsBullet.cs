a071bd64f29e611637a5eedb63dbfe99ac961a43
lip ables <phillipables1@gmail.com>
2018-06-16 21:13:26 -0400

    max distance plus equals delta time on update, if greater then five destroy object

--- Nurf Bang/Assets/TopDownBalls/Scripts/BallsBullet.cs
+++ Nurf Bang/Assets/TopDownBalls/Scripts/BallsBullet.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class BallsBullet : MonoBehaviour {
     //Variables
     public float bulletSpeed = 7.0f;
+    public float maxDistance;
 
     //Method
     private void Update()
     {
         transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+        maxDistance += 1 * Time.deltaTime;
+
+        if(maxDistance >= 5)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
