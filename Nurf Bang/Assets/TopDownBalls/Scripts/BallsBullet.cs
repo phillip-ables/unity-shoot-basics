fd53a8e99bcf5b137d09105428fcfd5fbaea2255
lip ables <phillipables1@gmail.com>
2018-06-16 21:07:28 -0400

    bullet is triggered, on update bullet moves forward

--- Nurf Bang/Assets/TopDownBalls/Scripts/BallsBullet.cs
+++ Nurf Bang/Assets/TopDownBalls/Scripts/BallsBullet.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallsBullet : MonoBehaviour {
-    
+    //Variables
+    public float bulletSpeed = 7.0f;
+
+    //Method
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+    }
 }
