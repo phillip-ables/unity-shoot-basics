b6b365454f88f83888e6aa12ebacfc588f7c2f30
lip ables <phillipables1@gmail.com>
2018-06-20 07:23:03 -0400

    on update if enemy distance to player is greater then stopping distance then enmy moves towards player, else if distance is less then stopping distance just refresh his position to be the same

--- Nurf Bang/Assets/BallsEnemyMove.cs
+++ Nurf Bang/Assets/BallsEnemyMove.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallsEnemyMove : MonoBehaviour {
+    //Variables
+    public float enemySpeed;
+    public float stoppingDistance;
+    public float retreatDistance;
+    public Transform player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Methods
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(player.position, gameObject.transform.position) > stoppingDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
+        }
+        else if(Vector3.Distance(player.position, gameObject.transform.position) < retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+    }
 }
