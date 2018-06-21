df5f35d715bc3a07c17f1df7f640eb4fd89cfa38
lip ables <phillipables1@gmail.com>
2018-06-21 07:08:35 -0400

    public int gunDamage, floats fireRate weapon range and hit force, public transform for gun end, private camera and wait for seconds of new wait for seconds .07f, audio source line renderer and float for next fire

--- Nurf Bang/Assets/FPS_BOX/Scripts/fpsRaycastShoot.cs
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsRaycastShoot.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class fpsRaycastShoot : MonoBehaviour {
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private AudioSource shotAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+
 }
