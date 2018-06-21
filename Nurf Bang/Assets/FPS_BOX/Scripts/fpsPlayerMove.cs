c6fab3e2724927e463c2db4ccbd3596815c7ed56
lip ables <phillipables1@gmail.com>
2018-06-21 04:28:14 -0400

    terrain and player set up, player move script added to camera which is now the parent of player holder, public game object player, float turn move and jump, bool is running and aiming

---
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsPlayerMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsPlayerMove : MonoBehaviour {
+    public GameObject player;
+    public float turnSpeed;
+    public float moveSpeed;
+    public float jumpStrength;
+    public bool isRunning;
+    public bool isAiming;
+
+
+}
