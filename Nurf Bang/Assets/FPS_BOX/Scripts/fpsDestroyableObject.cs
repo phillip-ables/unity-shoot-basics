1d9880d1a9a5d9ba299a2c4085a7e950062b98fb
lip ables <phillipables1@gmail.com>
2018-06-21 08:35:55 -0400

    public int current health and public void damage that takes an argument of dmaage amount, current health -= damage amount, if current health is less then 0 game object set active to false

--- Nurf Bang/Assets/FPS_BOX/Scripts/fpsDestroyableObject.cs
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsDestroyableObject.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class fpsDestroyableObject : MonoBehaviour {
+    public int currentHealth = 3;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Damage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if(currentHealth < 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
