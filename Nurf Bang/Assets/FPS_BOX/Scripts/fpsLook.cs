44fe6522fc4c0f62df7b98e5d9a31aff09a7188b
lip ables <phillipables1@gmail.com>
2018-06-22 06:46:44 -0400

    x rotation is clamped

--- Nurf Bang/Assets/FPS_BOX/Scripts/fpsLook.cs
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsLook.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class fpsLook : MonoBehaviour {
     //Variables
     public float lookSensitivity = 5.0f;
-    public float yRotation;
-    public float xRotation;
-    public float currentYRotation;
-    public float currentXRotation;
-    public float yRotationVelocity;
-    public float xRotationVelocity;
     public float lookSmoothDamp = 0.1f;
+
+    private float yRotation;
+    private float xRotation;
+    private float currentYRotation;
+    private float currentXRotation;
+    private float yRotationVelocity;
+    private float xRotationVelocity;
 
     //Methods
     private void Update()
     {
         yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
         xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, 90, -90);
 
         currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationVelocity, lookSmoothDamp);
         currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationVelocity, lookSmoothDamp);
