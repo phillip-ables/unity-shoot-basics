bb72869ef2307db943eb6f2af62834d4311b1493
lip ables <phillipables1@gmail.com>
2018-06-22 05:23:47 -0400

    if left mouse down cast ray from screen orgin to ray range

--- Nurf Bang/Assets/SimpleRaycast/Scripts/RayRaycast.cs
+++ Nurf Bang/Assets/SimpleRaycast/Scripts/RayRaycast.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RayRaycast : MonoBehaviour {
+    public float rayRange = 25;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Update()
+    {
+        //if left mouse button is clicked
+        //create a raycast that originates from the mouse click position
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo, rayRange))
+            {
+                Debug.Log("You are casting a ray");
+            }
+        }
+    }
 }
