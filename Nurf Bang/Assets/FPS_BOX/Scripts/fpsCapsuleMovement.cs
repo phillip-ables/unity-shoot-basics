ed64dea2622a10dc5626c5976281970ba684ac80
lip ables <phillipables1@gmail.com>
2018-06-22 08:10:20 -0400

    public float for max walk speed and vector2 for horizontal movement on update set the vector 2 to x and z velocities

--- Nurf Bang/Assets/FPS_BOX/Scripts/fpsCapsuleMovement.cs
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsCapsuleMovement.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class fpsCapsuleMovement : MonoBehaviour {
     public float walkAcceleration = 5.0f;
     public GameObject cameraObject;
+    public float maxWalkSpeed = 50.0f;
+
+    private Rigidbody rb;
+    private Vector2 horizontalMovement;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
+        horizontalMovement = new Vector2(rb.velocity.x, rb.velocity.z);
+
         transform.rotation = Quaternion.Euler(0, cameraObject.GetComponent<fpsLook>().currentYRotation, 0);
-        gameObject.GetComponent<Rigidbody>().AddRelativeForce(Input.GetAxis("Horizontal") * walkAcceleration, 0, Input.GetAxis("Vertical") * walkAcceleration);
+        rb.AddRelativeForce(Input.GetAxis("Horizontal") * walkAcceleration, 0, Input.GetAxis("Vertical") * walkAcceleration);
     }
 }
