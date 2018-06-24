ae800d61b38b6a5a60ab61879b2d7ef964213487
lip ables <phillipables1@gmail.com>
2018-06-23 21:36:27 -0400

    public float for move speed, transform array, on start get random spot of move spot array length

--- Nurf Bang/Assets/FPS_BOX/Scripts/fpsPatrol.cs
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsPatrol.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class fpsPatrol : MonoBehaviour {
-    
+    public float speed;
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    private void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
 }
