33e3a6baf16727c164a6f14fa468a013a3f94015
lip ables <phillipables1@gmail.com>
2018-06-23 21:25:17 -0400

    ghost monster three d object brought in and script added to the floating objects and they now c0ver the map, script added that instantes monsters on these spots

---
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsGhostSpwan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsGhostSpwan : MonoBehaviour {
+    public GameObject ghost;
+
+    private void Start()
+    {
+        Debug.Log(transform.childCount);
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Instantiate(ghost, transform.GetChild(i).position, Quaternion.identity);
+            Debug.Log(transform.GetChild(i).position);
+        }
+    }
+}
