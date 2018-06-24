2ce55a3447bac7c3fe3a548d51a36588a309656b
lip ables <phillipables1@gmail.com>
2018-06-23 22:01:51 -0400

    on update if distance from position to move spot is 0, public float start wait time, private float wiat Time

--- Nurf Bang/Assets/FPS_BOX/Scripts/fpsPatrol.cs
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsPatrol.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class fpsPatrol : MonoBehaviour {
     public float speed;
     public Transform[] moveSpots;
+    public float startWaitTime;
+
+    private float waitTime;
     private int randomSpot;
 
     private void Start()
     {
         randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+
+        }
     }
 }
