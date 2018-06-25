3c68ed11f830a1ff79d75871b4b47811fb713cd5
lip ables <phillipables1@gmail.com>
2018-06-25 04:08:40 -0400

    new scene with dialog system for opening scene

--- Nurf Bang/Assets/FPS_BOX/Scripts/fpsDialog.cs
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsDialog.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class fpsDialog : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //how many filters do you hide behind
+
+    //does he know you call me daddy
+    //or that youve wore me on your face in ways he cant understand
+    //how do you smile knowing what youve done
+
+    //she runs away from the voice
+    //down the coradoor there is a cat
+    //their eyes inbrace as it ruffly begins to speak
+
+    //hey, lets get the fuck out of here
+
+    //who are you
+    //in second grade his best friend got a cat
+    //he wanted one but you cant own pets in the projects
+    //ive been here ever since
+    //lets get out of here
+
+    //<RUN> or <RUN FASTER>
 }
