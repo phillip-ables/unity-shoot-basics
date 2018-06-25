d65e7ea0459ac6c88f8188397216a5227925bf4c
lip ables <phillipables1@gmail.com>
2018-06-25 09:25:58 -0400

    using scene management if enemies child count is 0 then load scene of public string scene name

--- Nurf Bang/Assets/TopDownBalls/Scripts/BallsSceneTransition.cs
+++ Nurf Bang/Assets/TopDownBalls/Scripts/BallsSceneTransition.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallsSceneTransition : MonoBehaviour {
     public Animator sceneAnimation;
     public Transform enemies;
+    public string sceneName;
 
-    private void Start()
+    private void Update()
     {
-        Debug.Log(enemies.childCount);
+        if(enemies.childCount == 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
