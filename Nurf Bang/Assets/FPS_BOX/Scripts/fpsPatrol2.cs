c4e0c41fe6ca075902f17d8735584f6fc107396c
lip ables <phillipables1@gmail.com>
2018-06-24 22:38:35 -0400

    picks random spot after wait time and moves theie

--- Nurf Bang/Assets/FPS_BOX/Scripts/fpsPatrol2.cs
+++ Nurf Bang/Assets/FPS_BOX/Scripts/fpsPatrol2.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class fpsPatrol2 : MonoBehaviour {
-    public float speed;
-    public float startWaitTime;
+    public float speed = 40.0f;
+    public float startWaitTime = 3.0f;
     public float minX = -48.0f;
     public float maxX = 442.0f;
     public float minY =   7.6f;
@@ -17,17 +17,19 @@ public class fpsPatrol2 : MonoBehaviour {
     private void Start()
     {
         waitTime = startWaitTime;
+        moveSpot.position = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minY, maxY));
     }
 
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, moveSpot.position) < 0.2f)
+        if (Vector3.Distance(transform.position, moveSpot.position) <= 0.2f)
         {
             if (waitTime <= 0)
             {
                 waitTime = startWaitTime;
+                moveSpot.position = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minY, maxY));
             }
             else
             {
