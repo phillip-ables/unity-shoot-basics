0ce2724ceda5f86098a65788e6c54648c75ed66b
lip ables <phillipables1@gmail.com>
2018-06-25 04:29:38 -0400

    sentences added to sentences array

--- Nurf Bang/Assets/TopDownBalls/Scripts/BallsDialog.cs
+++ Nurf Bang/Assets/TopDownBalls/Scripts/BallsDialog.cs
@@ -10,8 +10,18 @@ public class BallsDialog : MonoBehaviour {
 
     private string[] sentences;
     private int index;
-
     //Methods
+    private void Awake()
+    {
+        sentences = new string[] {
+            "!HoW MaNy FiLtErS dO yOu HiDe BeHiNd My LoVe?",
+            "!DOES HE KNOW YOU CALL ME DADDY?",
+            "or that youve wore me on your face in ways he cant understand",
+            "how do you smile knowing what youve done"
+        };
+    }
+
+    
     IEnumerator Type()
     {
         foreach(char letter in sentences[index].ToCharArray())
