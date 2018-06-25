c30923465222a4ceb38b3e20a578f1fce5aab7e8
lip ables <phillipables1@gmail.com>
2018-06-25 08:13:43 -0400

    empty game object for parent of camera, this is moved on coroutine in cam shake script (not sure how much i love this but it works

--- Nurf Bang/Assets/TopDownBalls/Scripts/BallsCameraFX.cs
+++ Nurf Bang/Assets/TopDownBalls/Scripts/BallsCameraFX.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class BallsCameraFX : MonoBehaviour {
     public Animator camShakeAnimation;
+    public float walkTime = 0.07f;
+    public Transform camFollow;
+
+    private int moveDistance = 5;
+    private Vector3 currentPos;
 
     public void CamShake()
     {
         camShakeAnimation.SetTrigger("Shake");
+        StartCoroutine(WalkDownTheHall());
+    }
+
+    IEnumerator WalkDownTheHall()
+    {
+        for(int i = 0; i < moveDistance; i++)
+        {
+            currentPos = camFollow.position;
+            currentPos.z += 1.0f;
+            yield return new WaitForSeconds(walkTime);
+            camFollow.position = currentPos;
+        }
     }
 }
