5ce30792828b8a75e80c4633142d100d707603ed
lip ables <phillipables1@gmail.com>
2018-06-25 04:18:37 -0400

    IEnumerator Type which runs a for each letter in sentences array adds a letter to the text display text then yeilds for typing speed variable

--- Nurf Bang/Assets/TopDownBalls/Scripts/BallsDialog.cs
+++ Nurf Bang/Assets/TopDownBalls/Scripts/BallsDialog.cs
@@ -6,8 +6,18 @@ using TMPro;
 public class BallsDialog : MonoBehaviour {
     //Variables
     public TextMeshProUGUI textDisplay;
-    public string[] sentences;
+    public float typingSpeed = 0.02f;
+
+    private string[] sentences;
     private int index;
 
     //Methods
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
 }
